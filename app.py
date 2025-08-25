73c4b0b0456953fba91d93833f4e8f6330478a6a
rajatk0007 <60063926+rajatk0007@users.noreply.github.com>
2025-08-26 02:07:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 import gradio as gr
 from src.rag import RAGPipeline
 from src.llm import hf_llm
@@ -6,9 +5,7 @@ from src.llm import hf_llm
 STORE_A_CSV = "data/store_a.csv"
 STORE_B_CSV = "data/store_b.csv"
 
-print("📚 Loading data and initializing pipeline...")
 rag = RAGPipeline.from_multiple_stores(STORE_A_CSV, STORE_B_CSV, llm=hf_llm)
-print("✅ Pipeline ready!")
 
 def answer_query(user_query):
     return rag.ask(user_query)
